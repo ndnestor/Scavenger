78eeafcb0919dcb159dfb275138aca9fcefb5dff
ndnestor <Nathan.nestor@outlook.com>
2021-10-24 13:25:11 -0400

    Moved some code from main.py to searcher.py

--- main.py
+++ main.py
@@ -1,94 +1,22 @@
 import sys
 import os
-from types import FunctionType
 
 import searcher
-from unity_searcher import UnitySearchModule
-
 import hotkey_handler
+from unity_searcher import UnitySearchModule
 
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
-from PySide6.QtCore import QObject, Property, QThread, Signal
 
+
+# Allow QML to read from local files
+os.environ['QML_XHR_ALLOW_FILE_READ'] = '1'
 
 # Searcher setup
 searcher.register_search_module(UnitySearchModule())
 
-
-class Searcher(QObject):
-    def __init__(self, parent=None) -> None:
-        super().__init__(parent)
-
-        self._query: str = ''
-        self._search_results: list[dict] = []
-        self._results_have_changed: bool = False
-        self._cards: list = []
-        self.last_searched_query: str = ''
-
-        def search_work() -> None:
-            self._search_results = searcher.search(self.query)
-            self._results_have_changed = True
-
-        self.search_worker = Worker(search_work)
-        self.search_thread = QThread()
-        self.search_worker.moveToThread(self.search_thread)
-        self.search_thread.started.connect(self.search_worker.run)
-        self.search_worker.finished.connect(self.search_thread.quit)
-
-    @Property(list)
-    def cards(self) -> list:
-        return self._cards
-    
-    @cards.setter
-    def cards(self, cards: list) -> None:
-        self._cards = cards
-    
-    @Property(bool)
-    def results_have_changed(self) -> bool:
-        return self._results_have_changed
-    
-    @results_have_changed.setter
-    def results_have_changed(self, results_have_changed: bool) -> None:
-        self._results_have_changed = results_have_changed
-
-    @Property(list)
-    def search_results(self) -> list[dict]:
-        return self._search_results
-    
-    @Property(str)
-    def query(self) -> str:
-        return self._query
-    
-    @query.setter
-    def query(self, query: str) -> None:
-
-        self._query = query
-
-        # Search if query has not been searched yet and thread is not in progress
-        if not self.search_thread.isRunning():
-            if not self.last_searched_query == query:
-                self.last_searched_query = query
-                self.search_thread.start()
-
-
-class Worker(QObject):
-    finished = Signal()
-    
-    def __init__(self, work: FunctionType, parent=None) -> None:
-        super().__init__(parent)
-        self.work = work
-
-    def run(self) -> None:
-        self.work()
-        self.finished.emit()
-
-
 # Qt application setup
 app = QGuiApplication(sys.argv)
-
-qmlRegisterType(Searcher, 'Searcher', 1, 0, 'Searcher')
-
 engine = QQmlApplicationEngine()
 engine.load(os.path.join(os.path.dirname(__file__), 'qml/main.qml'))
 
--- searcher.py
+++ searcher.py
@@ -1,5 +1,11 @@
 from search_module import SearchModule
 
+from types import FunctionType
+
+from PySide6.QtQml import qmlRegisterType
+from PySide6.QtCore import QObject, Property, QThread, Signal
+
+
 search_modules: list[SearchModule] = []
 
 
@@ -13,5 +19,76 @@ def register_search_module(search_module: SearchModule):
 
 
 def search(query: str) -> list[dict]:
+    print('Searching for ' + query)
     for search_module in search_modules:
         return search_module.search(query)
+
+
+class Searcher(QObject):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+
+        self._query: str = ''
+        self._search_results: list[dict] = []
+        self._results_have_changed: bool = False
+        self._cards: list = []
+        self.last_searched_query: str = ''
+
+        def search_work() -> None:
+            self._search_results = search(self.query)
+            self._results_have_changed = True
+
+        self.search_worker = Worker(search_work)
+        self.search_thread = QThread()
+        self.search_worker.moveToThread(self.search_thread)
+        self.search_thread.started.connect(self.search_worker.run)
+        self.search_worker.finished.connect(self.search_thread.quit)
+
+    @Property(list)
+    def cards(self) -> list:
+        return self._cards
+    
+    @cards.setter
+    def cards(self, cards: list) -> None:
+        self._cards = cards
+    
+    @Property(bool)
+    def results_have_changed(self) -> bool:
+        return self._results_have_changed
+    
+    @results_have_changed.setter
+    def results_have_changed(self, results_have_changed: bool) -> None:
+        self._results_have_changed = results_have_changed
+
+    @Property(list)
+    def search_results(self) -> list[dict]:
+        return self._search_results
+    
+    @Property(str)
+    def query(self) -> str:
+        return self._query
+    
+    @query.setter
+    def query(self, query: str) -> None:
+
+        self._query = query
+
+        # Search if query has not been searched yet and thread is not in progress
+        if not self.search_thread.isRunning():
+            if not self.last_searched_query == query:
+                self.last_searched_query = query
+                self.search_thread.start()
+
+
+class Worker(QObject):
+    finished = Signal()
+    
+    def __init__(self, work: FunctionType, parent=None) -> None:
+        super().__init__(parent)
+        self.work = work
+
+    def run(self) -> None:
+        self.work()
+        self.finished.emit()
+
+qmlRegisterType(Searcher, 'Searcher', 1, 0, 'Searcher')
