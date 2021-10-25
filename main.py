568f511080434bf3031417c3bb2e83d1e2217009
ndnestor <nathan.nestor@rutgers.edu>
2021-10-25 00:33:14 -0400

    Removed extra import statement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import hotkey_handler
 from unity_searcher import UnitySearchModule
 
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
+from PySide6.QtQml import QQmlApplicationEngine
 
 
 # Allow QML to read from local files
@@ -19,7 +19,6 @@ searcher.register_search_module(UnitySearchModule())
 app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
 engine.load(os.path.join(os.path.dirname(__file__), 'qml/main.qml'))
-
 if not engine.rootObjects():
     sys.exit(-1)
 
